6a2ad804be29272cee15cd0a78d60dc8b51595d2
aishwarya <aishwaryar@valueaddsofttech.com>
2024-08-26 11:40:12 +0530

    changes in articles service and avgRatings for doctor

--- src/prescription/prescription.controller.ts
+++ src/prescription/prescription.controller.ts
@@ -1,11 +1,23 @@
 // prescription.controller.ts
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  BadRequestException,
+} from '@nestjs/common';
 import { PrescriptionService } from './prescription.service';
 import { CreatePrescriptionDto } from './dtos/create-prescription.dto';
 import { UpdatePrescriptionDto } from './dtos/update-prescription.dto';
+import { Types } from 'mongoose';
+
 import { from } from 'rxjs';
 import { query } from 'express';
-import { Prescription } from 'src/schemas/Prescription.schema';
+import { Medicine, Prescription } from 'src/schemas/Prescription.schema';
 
 @Controller('prescriptions')
 export class PrescriptionController {
@@ -15,7 +27,42 @@ export class PrescriptionController {
   create(@Body() createPrescriptionDto: CreatePrescriptionDto) {
     return this.prescriptionService.create(createPrescriptionDto);
   }
+  @Post('/save')
+  async savePrescription(
+    @Body()
+    prescriptionDto: {
+      patientId: string;
+      doctorId: string;
+      doctorName: string;
+      patientName: string;
+      note: string;
+      medicines: {
+        name: string;
+        dosage: string[];
+        time: string;
+        days: number;
+      }[];
+      appointmentDate: string;
+      slotId: string;
+    },
+  ): Promise<Prescription> {
+    // Log the incoming request body for debugging
+    console.log('Saving prescription:', prescriptionDto);
+    try {
+      const transformedPrescriptionDto = {
+        ...prescriptionDto,
+        doctorId: new Types.ObjectId(prescriptionDto.doctorId),
+        patientId: new Types.ObjectId(prescriptionDto.patientId),
+        appointmentDate: new Date(prescriptionDto.appointmentDate),
+        slotId: new Types.ObjectId(prescriptionDto.slotId),
+      };
+      return this.prescriptionService.savePrescription(prescriptionDto);
+    } catch (error) {
+      throw new BadRequestException('Invalid data provided');
+    }
 
+    // Save the prescription using the service
+  }
   @Get()
   async findAll(@Query() query: any): Promise<Prescription[]> {
     return this.prescriptionService.findAll(query);
@@ -26,7 +73,10 @@ export class PrescriptionController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePrescriptionDto: UpdatePrescriptionDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updatePrescriptionDto: UpdatePrescriptionDto,
+  ) {
     return this.prescriptionService.update(id, updatePrescriptionDto);
   }
 
